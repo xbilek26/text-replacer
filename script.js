46233d2eb063a5d41d07837f99bc994b09e942d4
František Bílek <bilekf@email.cz>
2024-03-21 21:27:39 +0100

    Upload files

---
+++ script.js
@@ -0,0 +1,24 @@
+function replaceText() {
+    var inputText = document.getElementById("inputText").value;
+    var replaceText = document.getElementById("replaceText").value;
+    var replacementText = document.getElementById("replacementText").value;
+    var caseSensitive = document.getElementById("caseSensitive").checked;
+
+    var output = document.getElementById("output");
+
+    var flags = "g";
+    if (!caseSensitive) {
+        flags += "i";
+    }
+
+    var replacedText = inputText.replace(new RegExp(replaceText, flags), replacementText);
+
+    output.value = replacedText;
+}
+
+document.getElementById("inputText").addEventListener("input", replaceText);
+document.getElementById("replaceText").addEventListener("input", replaceText);
+document.getElementById("replacementText").addEventListener("input", replaceText);
+document.getElementById("caseSensitive").addEventListener("change", replaceText);
+
+replaceText();
